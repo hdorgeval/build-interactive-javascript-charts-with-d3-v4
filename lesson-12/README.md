f7177cdaacc93c0e943003da6a42a6709f1b226a
Henri Le Barrois d'Orgeval <HDO@MacBook.local>
2017-10-19 13:15:14 +0200

    feat(lesson12): Create DOM Elements with D3 v4

---
+++ lesson-12/README.md
@@ -0,0 +1,10 @@
+# build-interactive-javascript-charts-with-d3-v4
+
+Lesson 12 - Create DOM Elements with D3 v4
+
+To start Lesson 12 from scratch, copy index.starter.html to index.html and app.starter.js to app.js.
+
+To run lesson 12:
+```
+npm run lesson12
+```
---
+++ lesson-12/src/app.final.js
@@ -0,0 +1,28 @@
+// append a DOM element inside another DOM element
+// in this example I append a button inside a div
+d3.select("div.title")
+.append("button")
+  .html("Buy Now <b>Special Offer</b>");  
+  
+// insert a DOM element before another one
+d3.select("div.title")
+.insert("button","a:first-child")
+  .html("inserted before the first link");  
+
+// insert a DOM element before the second link
+d3.select("div.title")
+.insert("button","a:nth-child(3)")
+  .html("inserted before the second link"); 
+
+//remove the link with class action
+d3.select("a.action")
+.remove();
+
+
+//insert a complete hierarchy of DOM elements
+d3.select("body")
+.append("div")
+  .classed("chart",true)
+.append("button")
+  .style("color","red")
+  .text("submit")
